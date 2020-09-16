9c78cbca3233dc289b65832499103e58e153e7d1
Adam Sears <asears2@live.com>
2020-09-16 02:20:23 -0500

    Modified the comments in ViewModelBase to be a bit more clear on what they're doing

--- WpfSheet/ViewModels/ViewModelBase.cs
+++ WpfSheet/ViewModels/ViewModelBase.cs
@@ -25,8 +25,15 @@ namespace WpfSheet.ViewModels
         /// <param name="property">The name of the property that is being updated</param>
         /// <param name="oldValue">A reference to the current value</param>
         /// <param name="newValue">The new value to change to</param>
+        /// <param name="propertyChanged">A custom method to invoke in addition to <see cref="OnPropertyChanged(string)"/></param>
+        /// <param name="propertyChanging">A custom method to invoke in addition to <see cref="OnPropertyChanging(string)"/></param>
         /// <param name="others">Additional properties to be invoked</param>
-        /// <remarks>If <paramref name="oldValue"/> and <paramref name="newValue"/> are identical then no change notification events occur</remarks>
+        /// <remarks>
+        ///     If <paramref name="oldValue"/> and <paramref name="newValue"/> are identical then no change notification events occur.
+        ///     Additionally, the built in notification methods are invoked first BEFORE the custom handlers.
+        ///     If the handlers for <see cref="OnPropertyChanging(string)"/> or <paramref name="propertyChanging"/> throw an Exception, the property WILL NOT CHANGE.
+        ///     If the handlers for <see cref="OnPropertyChanged(string)"/> or <paramref name="propertyChanged"/> throw an Exception, the <paramref name="others"/> collection will not be iterated.
+        /// </remarks>
         internal void Set<T>(
             string property,
             ref T oldValue,
@@ -44,12 +51,22 @@ namespace WpfSheet.ViewModels
             foreach (var other in others) OnPropertyChanged(other);
         }
 
+        /// <summary>
+        ///     Raises <see cref="INotifyPropertyChanged.PropertyChanged"/> for the supplied property
+        /// </summary>
+        /// <param name="property">The name of the property that has been changed</param>
+        /// <remarks>This method should be invoked AFTER the data in the property has been altered</remarks>
         internal void OnPropertyChanged(string property)
         {
             var pc = PropertyChanged;
             pc?.Invoke(this, new PropertyChangedEventArgs(property));
         }
 
+        /// <summary>
+        ///     Raises <see cref="INotifyPropertyChanging.PropertyChanging"/> for the supplied property
+        /// </summary>
+        /// <param name="property">The name of the property that has been changed</param>
+        /// <remarks>This method should be invoked BEFORE the data in the property has been altered</remarks>
         internal void OnPropertyChanging(string property)
         {
             var pc = PropertyChanging;
